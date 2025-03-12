73fa843536c0d55d88d3c66374063f9d60add0a1
Sachin <sachinkaushikofficial@gmail.com>
2025-03-12 18:12:42 +0530

    initial commit

---
+++ src/main/java/com/sachinsk/Stereotype_Annotation/StereotypeAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.sachinsk.Stereotype_Annotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StereotypeAnnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StereotypeAnnotationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sachinsk/Stereotype_Annotation/StereotypeAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachinsk.Stereotype_Annotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StereotypeAnnotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
